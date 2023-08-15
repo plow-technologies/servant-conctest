5a723c2ab9d94b5f66e75ffaee37c923a3a54008
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-08-15 14:01:32 +0200

    Bring over code to pre compute seq results and test concurrently

--- app/Main.hs
+++ app/Main.hs
@@ -8,25 +8,18 @@
 
 module Main (main) where
 
-import Control.Concurrent.MVar (newMVar, putMVar, readMVar, takeMVar)
-import Control.Monad (forM_, when)
+import Control.Concurrent.Async (forConcurrently)
+import Control.Concurrent.MVar
+import Control.Monad (forM, forM_, when)
 import Control.Monad.IO.Class (liftIO)
+import Data.Either (partitionEithers)
 import Data.List (intercalate)
 import Network.HTTP.Client (defaultManagerSettings, newManager)
 import Network.Wai.Handler.Warp (defaultSettings, withApplicationSettings)
 import Servant
-  ( Context (EmptyContext),
-    Get,
-    Handler,
-    JSON,
-    Post,
-    Proxy (..),
-    Server,
-    serveWithContext,
-    type (:<|>) (..),
-  )
 import Servant.Client (BaseUrl (..), ClientM, Scheme (Http), client, mkClientEnv, runClientM)
 import System.Exit (exitFailure)
+import Test.Hspec
 
 type API = Get '[JSON] Int :<|> Post '[JSON] ()
 
@@ -60,30 +53,85 @@ getClient :: ClientM Int
 postClient :: ClientM ()
 getClient :<|> postClient = client api
 
+-- | Generate all possible interleavings of the input lists.
+-- From https://stackoverflow.com/a/41929156
+-- NOTE: is there a more efficient way to do this?
+interleavings :: [[a]] -> [[a]]
+interleavings = go . filter (not . null)
+  where
+    go [] = [[]]
+    go xss = do
+      (xssl, x : xs, xssr) <- zippers xss
+      (x :) <$> interleavings ([xs | not (null xs)] ++ xssl ++ xssr)
+    zippers :: [a] -> [([a], a, [a])]
+    zippers = go' []
+      where
+        go' l (h : r) = (l, h, r) : go' (h : l) r
+        go' _ [] = []
+
+-- | Convert a list of tuples to a list of lists, using the integers as indexes
+-- in the returned list
+-- TODO test that gather is the inverse of interleavings
+gather :: [(Int, a)] -> [[a]]
+gather [] = []
+gather ((i, x) : xs) = insert i x (gather xs)
+
+-- | Insert an element into the list at a given index
+insert :: Int -> a -> [[a]] -> [[a]]
+-- If the index is zero, prepend the element to the first sublist or create a new sublist if the list is empty
+insert 0 x [] = [[x]]
+insert 0 x (l : ls) = (x : l) : ls
+-- If the index is negative, return the original list
+insert i _ ls | i < 0 = ls
+-- If the index is positive, recurse on the tail of the list and decrement the index
+insert i x (l : ls) = l : insert (i - 1) x ls
+-- If the index is larger than the length of the list, append the element to the last sublist or create a new sublist if the list is empty
+insert i x [] = replicate i [] ++ [[x]]
+
+withServantServer :: HasServer api '[] => Proxy api -> IO (ServerT api Handler) -> (BaseUrl -> IO a) -> IO a
+withServantServer api server fn =
+  withApplicationSettings defaultSettings (return . serveWithContext api EmptyContext =<< server) $ \port ->
+    fn $ BaseUrl Http "localhost" port ""
+
 main :: IO ()
 main = do
   manager <- newManager defaultManagerSettings
 
+  -- Define the concurrent execution to be tested
   let getClientFn = show <$> getClient
   let postClientFn = show <$> postClient
-  let exec :: [ClientM String] = [postClientFn, postClientFn, getClientFn]
+  let exec :: [[ClientM String]] = [[postClientFn, postClientFn], [getClientFn]]
 
-  let stringifyResults results = intercalate "," results
+  -- Generate all sequential interleavings and compute expected results
+  let seqExecs :: [[(Int, ClientM String)]] =
+        -- Add the thread ID to each invocation before interleaving:
+        interleavings $ map (\(t, es) -> zip (repeat t) es) $ zip [0 ..] exec
+  seqResults <- forM seqExecs $ \seqExec -> do
+    withServantServer api server $ \burl -> do
+      let runFnWithThreadID (t, f) = (t,) <$> f
+      let seqClient = sequence $ map runFnWithThreadID seqExec
+      runClientM seqClient (mkClientEnv manager burl) >>= \case
+        Left err -> error $ "Error computing expected results:\n" ++ show err
+        Right res -> return res
+  -- Gather the results and stringify for easy comparison
+  let stringifyResults results =
+        intercalate "|" $ map (intercalate ",") results
+  let expectedResults = map (stringifyResults . gather) seqResults
+  print expectedResults -- TODO remove
 
-  -- Run test 5000 times in order to make it more likely to see concurrency errors
+  -- Run concurrently and check results are as expected
   forM_ [1 .. 5000 :: Int] $ \i -> do
-    -- Create the server anew each test because then we can compare the results of the client calls with an expected set
-    withApplicationSettings defaultSettings (return . serveWithContext api EmptyContext =<< server) $ \port -> do
-      let burl = BaseUrl Http "localhost" port ""
-      -- For brevity I'm calling the API sequentially here, but the error occurs when I call it concurrently too
-      res <- runClientM (sequence exec) $ mkClientEnv manager burl
-
-      case res of
-        Right results ->
-          -- This is a dummy line to force evaluation, in reality we'd check that the results satisfy some test condition here:
-          if length (stringifyResults results) > 1000 then print i else pure ()
-        Left err -> do
+    -- Create the server anew each test because then we can compare the results with the expected set
+    withServantServer api server $ \burl -> do
+      res <- forConcurrently exec $ \thredFns ->
+        runClientM (sequence thredFns) $ mkClientEnv manager burl
+      case partitionEithers res of
+        ([], results) ->
+          shouldContain expectedResults [stringifyResults results]
+        -- print $ (i, stringifyResults results)
+        -- if length (stringifyResults results) > 1000 then print i else pure ()
+        (errs, _) -> do
           print i
-          putStrLn $ "There was an error:\n" ++ show err
+          putStrLn $ "There was an error:\n" ++ show errs
           exitFailure
       return ()
