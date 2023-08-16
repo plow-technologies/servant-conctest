5668b12b05ce11af9aa09401ed16f3adb47de086
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-08-16 17:31:25 +0200

    Add a reset function and use it to speed up

--- app/Main.hs
+++ app/Main.hs
@@ -21,37 +21,48 @@ import Servant.Client (BaseUrl (..), ClientM, Scheme (Http), client, mkClientEnv
 import System.Exit (exitFailure)
 import Test.Hspec
 
-type API = Get '[JSON] Int :<|> Post '[JSON] ()
+type API = "reset" :> Post '[JSON] ()
+  :<|> "get" :> Get '[JSON] Int
+  :<|> "put" :> Post '[JSON] ()
 
 api :: Proxy API
 api = Proxy
 
 -- 2 MVars, get reads both in turn without locks
--- post increments both in turn without locks
+-- put increments both in turn without locks
 server :: IO (Server API)
 server = do
   mvar1 <- newMVar 0
   mvar2 <- newMVar 0
   let getFn :: Handler Int
       getFn = do
+        -- liftIO $ putStrLn "get"
         x1 <- liftIO $ readMVar mvar1
         x2 <- liftIO $ readMVar mvar2
         when (odd (x1 + x2)) $ do
           liftIO $ putStrLn "ERROR: not even"
         --   throwError err500 {errBody = "Not even"}
         return $ x1 + x2
-  let postFn :: Handler ()
-      postFn = do
+  let putFn :: Handler ()
+      putFn = do
+        -- liftIO $ putStrLn "put"
         x1 <- liftIO $ takeMVar mvar1
         liftIO $ putMVar mvar1 (x1 + 1)
         x2 <- liftIO $ takeMVar mvar2
         liftIO $ putMVar mvar2 (x2 + 1)
         return ()
-  return $ getFn :<|> postFn
+  let resetFn :: Handler ()
+      resetFn = do
+        -- liftIO $ putStrLn "reset"
+        _ <- liftIO $ swapMVar mvar1 0
+        _ <- liftIO $ swapMVar mvar2 0
+        return ()
+  return $ resetFn :<|> getFn :<|> putFn
 
+resetClient :: ClientM ()
 getClient :: ClientM Int
-postClient :: ClientM ()
-getClient :<|> postClient = client api
+putClient :: ClientM ()
+resetClient :<|> getClient :<|> putClient = client api
 
 -- | Generate all possible interleavings of the input lists.
 -- From https://stackoverflow.com/a/41929156
@@ -89,24 +100,32 @@ insert i x (l : ls) = l : insert (i - 1) x ls
 insert i x [] = replicate i [] ++ [[x]]
 
 withServantServer :: HasServer api '[] => Proxy api -> IO (ServerT api Handler) -> (BaseUrl -> IO a) -> IO a
-withServantServer api server fn =
+withServantServer api server fn = do
   withApplicationSettings defaultSettings (return . serveWithContext api EmptyContext =<< server) $ \port ->
     fn $ BaseUrl Http "localhost" port ""
 
+  -- NOTE: this might avoid the socket not found error, but is MUCH slower:
+  -- let port = 54321
+  -- srv <- server
+  -- withAsync (run port (serveWithContext api EmptyContext srv)) $ \_ -> do
+  --   fn $ BaseUrl Http "localhost" port ""
+
 main :: IO ()
 main = do
+  -- TODO is it okay to share manager among threads?
   manager <- newManager defaultManagerSettings
 
   -- Define the concurrent execution to be tested
   let getClientFn = show <$> getClient
-  let postClientFn = show <$> postClient
-  let exec :: [[ClientM String]] = [[postClientFn, postClientFn], [getClientFn]]
+  let putClientFn = show <$> putClient
+  let exec :: [[ClientM String]] = [[putClientFn, putClientFn], [getClientFn]]
 
   -- Generate all sequential interleavings and compute expected results
   let seqExecs :: [[(Int, ClientM String)]] =
         -- Add the thread ID to each invocation before interleaving:
         interleavings $ map (\(t, es) -> zip (repeat t) es) $ zip [0 ..] exec
   seqResults <- forM seqExecs $ \seqExec -> do
+    -- TODO also use reset here to speed pre-computation if necessary
     withServantServer api server $ \burl -> do
       let runFnWithThreadID (t, f) = (t,) <$> f
       let seqClient = sequence $ map runFnWithThreadID seqExec
@@ -120,18 +139,25 @@ main = do
   print expectedResults -- TODO remove
 
   -- Run concurrently and check results are as expected
-  forM_ [1 .. 5000 :: Int] $ \i -> do
-    -- Create the server anew each test because then we can compare the results with the expected set
-    withServantServer api server $ \burl -> do
-      res <- forConcurrently exec $ \thredFns ->
-        runClientM (sequence thredFns) $ mkClientEnv manager burl
-      case partitionEithers res of
-        ([], results) ->
-          shouldContain expectedResults [stringifyResults results]
-        -- print $ (i, stringifyResults results)
-        -- if length (stringifyResults results) > 1000 then print i else pure ()
-        (errs, _) -> do
-          print i
-          putStrLn $ "There was an error:\n" ++ show errs
+  withServantServer api server $ \burl -> do
+    forM_ [1 .. 1000 :: Int] $ \i -> do
+      let clientEnv = mkClientEnv manager burl
+      -- Reset server state
+      runClientM resetClient clientEnv >>= \case
+        Left err -> do
+          putStrLn $ "There was an error:\n" ++ show err
           exitFailure
+        Right () -> do
+          -- Run test execution concurrently
+          res <- forConcurrently exec $ \threadFns -> do
+            runClientM (sequence threadFns) clientEnv
+          case partitionEithers res of
+            ([], results) -> do
+              shouldContain expectedResults [stringifyResults results]
+              -- print $ (i, stringifyResults results)
+              -- if length (stringifyResults results) > 1000 then print i else pure ()
+            (errs, _) -> do
+              print i
+              putStrLn $ "There was an error:\n" ++ show errs
+              exitFailure
       return ()
