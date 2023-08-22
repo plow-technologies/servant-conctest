d048af7a05ff14a9a9fbd70c75d58f67c2cade2c
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-08-22 11:19:18 +0200

    Improve tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,19 +6,20 @@ import Debug.Trace (trace)
 import Servant.ConcTest.Utils
 import Test.QuickCheck
 
--- | If you take a list of lists, add the list index to each element, interleave it,
--- and then gather any of the interleavings, you get back the original list
-prop_interleave_then_gather_id :: (Show a, Eq a) => [[a]] -> Property
-prop_interleave_then_gather_id xs =
-  forAll (elements $ interleavings $ addListIndex xs) $ \ys ->
-    gather ys === xs
+main :: IO ()
+main = do
+  -- If you take a list of lists, add the list index to each element, interleave it,
+  -- and then gather any of the interleavings, you get back the original list
+  let listOfLists :: Gen [[Char]] =
+        resize 4 $ listOf $ listOf1 $ choose ('a', 'z')
+  let listAndInterleavings :: Gen ([[Char]], [[(Int, Char)]]) = do
+        l <- listOfLists
+        return (l, interleavings $ addListIndex l)
+  -- quickCheck $ verbose $ forAll listAndInterleavings $ \(xs, yss) ->
+  quickCheck $ forAll listAndInterleavings $ \(xs, yss) ->
+    conjoin [gather ys === xs | ys <- yss]
   where
     -- Convert each list into pairs by zipping the list index to each element:
     -- [[a, b], [c], [d, e]] ~> [[(0, a), (0, b)], [(1, c)], [(2, d), (2, e)]]
     addListIndex xs' | trace ("xs': " ++ show xs') False = undefined
     addListIndex xs' = map (\(t, es) -> zip (repeat t) es) . (zip [0 ..]) $ xs'
-
-main :: IO ()
-main = do
-  let listOfLists :: Gen [[Char]] = resize 3 $ listOf $ listOf1 $ choose ('a', 'z')
-  quickCheck $ verbose $ forAll listOfLists prop_interleave_then_gather_id
